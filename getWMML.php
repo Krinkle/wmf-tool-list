4ffdaa9ed0c5093a5b00d44dfa7b1374ec9275ec
Timo Tijhof <krinklemail@gmail.com>
2014-05-21 00:20:31 +0200

    Refactor and clean up

--- getWMML.php
+++ getWMML.php
@@ -1,142 +1,135 @@
 <?php
 /**
- * getWMML.php :: All-in-One file
+ * Wikimedia Mailing Lists utilities
  *
- * Get Wikimedia Mailing Lists
- * Created on February 1st, 2011
+ * Short urls:
+ * - wikitech-l:
+ *   - http://bit.ly/wikitechLast
+ *   - http://bit.ly/wikitechLatest
  *
- * Copyright 2011 Krinkle <krinklemail@gmail.com>
+ * - toolserver-l:
+ *   - http://bit.ly/toolserverLast
+ *   - http://bit.ly/toolserverLatest
+ *   - http://bit.ly/toolserverMonth
  *
- * This file is released in the public domain.
+ * - commons-l:
+ *   - http://bit.ly/commonsLast
+ *   - http://bit.ly/commonsMonth
+ *
+ * BEWARE: Ugly hacks ahead. Caution proceeding.
+ *
+ * @license http://krinkle.mit-license.org/
+ * @author Timo Tijhof, 2010-2014
  */
-/*
-        Shortened:
-        Wikitech-l:             <http://bit.ly/wikitechLast>    / <http://bit.ly/wikitechLatest>
-                                        <http://bit.ly/wikitechMonth>
-        Toolserver-l:   <http://bit.ly/toolserverLast>  / <http://bit.ly/toolserverLatest>
-                                        <http://bit.ly/toolserverMonth>
-        Commons-l:              <http://bit.ly/toolserverLast>
-                                        <http://bit.ly/toolserverMonth>
-*/
-
-
-
-
-/** BEWARE: Ugly hacks ahead. Caution proceeding. **/
-
-
-
 
 /**
  * Configuration
  * -------------------------------------------------
  */
-require_once( 'CommonStuff.php' );
-
-$c['revID'] = '0.0.2';
-$c['revDate'] = '2011-02-01';
-$c['title'] = 'Get Wikimedia Mailing Lists';
-$c['baseurl'] = $c['tshome'] .'/getWMML.php';
-
+require_once( __DIR__ . '/lib/basetool/InitTool.php' );
 
 /**
  * Functions
  * -------------------------------------------------
  */
+
 function injectScript( $scriptTag, $source ) {
-        if ( !is_string( $scriptTag ) || !is_string( $source ) ) {
-                die( 'Error while loading script.' );
-        }
-        return str_ireplace( '</body>', $scriptTag . '</body>', $source );
+	if ( !is_string( $scriptTag ) || !is_string( $source ) ) {
+		die( 'Error while loading script.' );
+	}
+	return str_ireplace( '</body>', $scriptTag . '</body>', $source );
 
 }
 
 function injectJQuery( $source ) {
-        return injectScript( '<script src="' . krGetjQueryURL() . '"></script>', $source );
+	return injectScript( '<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>', $source );
 }
 
 function downloadListPage( $list = false, $path = '' ) {
-        $url = 'http://lists.wikimedia.org/pipermail/' . rawurlencode( $list ) . '/' . $path;
-        $sourceCode = file_get_contents( $url );
-        if ( $sourceCode ) {
-                return $sourceCode;
-        } else {
-                die( 'Error while retrieving list index.' );
-        }
+	$url = 'http://lists.wikimedia.org/pipermail/' . rawurlencode( $list ) . '/' . $path;
+	$sourceCode = file_get_contents( $url );
+	if ( $sourceCode ) {
+		return $sourceCode;
+	} else {
+		die( 'Error while retrieving list index.' );
+	}
 }
 
-function injectGoToCurrMonth( $source ) {
-        global $params;
-        $replace = '$1';
-        $base = 'http://lists.wikimedia.org/pipermail/' . rawurlencode( $params['list'] ) . '/' . $replace;
-        $script = <<<SCRIPT
-        <script type="text/javascript">
-        jQuery( document ).ready( function( $ ) {
-                var currMonthLocation = $( 'a' ).eq( 4 ).attr( 'href' );
-                location.href='$base'.replace( '$replace', currMonthLocation );
-        } );
-        </script>
+function injectGoToCurrMonth( $source, $list ) {
+	$replace = '$1';
+	$base = 'http://lists.wikimedia.org/pipermail/' . rawurlencode( $list ) . '/' . $replace;
+	$script = <<<SCRIPT
+	<script>
+	jQuery(function ($) {
+		var currMonthLocation = $('a').eq(4).attr('href');
+		location.href = '$base'.replace('$replace', currMonthLocation);
+	});
+	</script>
 SCRIPT;
-        return injectScript( $script, $source );
+	return injectScript( $script, $source );
 }
 
-function injectGoToLastEntry_StepOne( $source ) {
-        global $params;
-        $p = array_merge( $params, array(
-                'action' => 'lastentry-processing',
-                'tmp' => '$1',
-        ) ) ;
-        $replace = rawurlencode( '$1' );
-        $base = generatePermalink( $p );
-        $script = <<<SCRIPT
-        <script type="text/javascript">
-        jQuery( document ).ready( function( $ ) {
-                var currMonthLocation = $( 'a' ).eq( 4 ).attr( 'href' );
-                location.href='$base'.replace( '$replace', currMonthLocation );
-        } );
-        </script>
+function injectGoToLastEntry_StepOne( $source, $params ) {
+	$p = array_merge( $params, array(
+		'action' => 'lastentry-processing',
+		'tmp' => '$1',
+	) ) ;
+	$replace = rawurlencode( '$1' );
+	$base = './getWMML.php?' . http_build_query( $p );
+	$script = <<<SCRIPT
+	<script>
+	jQuery(function ($) {
+		var currMonthLocation = $('a').eq(4).attr('href');
+		location.href = '$base'.replace('$replace', currMonthLocation);
+	});
+	</script>
 SCRIPT;
-        return injectScript( $script, $source );
+	return injectScript( $script, $source );
 }
 
-function injectGoToLastEntry_StepTwo( $source ) {
-        global $params;
-        $replace = rawurlencode( '$1' );
-        $base = 'http://lists.wikimedia.org/pipermail/' . rawurlencode( $params['list'] ) . '/' . rawurlencode( str_replace( '/date.html', '', $params['tmp'] ) ) . '/' . $replace;
-        $script = <<<SCRIPT
-        <script type="text/javascript">
-        jQuery( document ).ready( function( $ ) {
-                var lastEntryLocation = $( 'ul:eq(1) > li:last > a:first' ).attr( 'href' );
-                location.href='$base'.replace( '$replace', lastEntryLocation );
-        } );
-        </script>
+function injectGoToLastEntry_StepTwo( $source, $list, $tmp ) {
+	$replace = rawurlencode( '$1' );
+	$base = 'http://lists.wikimedia.org/pipermail/' .
+		rawurlencode( $list ) . '/' .
+		rawurlencode( str_replace( '/date.html', '', $tmp ) ) .
+		'/' .
+		$replace;
+
+	$script = <<<SCRIPT
+	<script>
+	jQuery(function ($) {
+		var lastEntryLocation = $('ul:eq(1) > li:last > a:first').attr('href');
+		location.href = '$base'.replace('$replace', lastEntryLocation);
+	});
+	</script>
 SCRIPT;
-        return injectScript( $script, $source );
+	return injectScript( $script, $source );
 }
-
 
 /**
- * Settings
+ * Variables
  * -------------------------------------------------
  */
-$s = array();
-$s['validActions'] = array( 'index', 'thismonth', 'lastentry', 'lastentry-processing' );
 
+$validActions = array( 'index', 'thismonth', 'lastentry', 'lastentry-processing' );
+
+$params = array(
+	'list' => $kgReq->getVal( 'list' ),
+	'action' => $kgReq->getVal( 'action' ),
+	'tmp' => $kgReq->getVal( 'tmp' ),
+);
 
 /**
- * Parameters
+ * Validation
  * -------------------------------------------------
  */
-$params['list'] = getParamVar( 'list' );
-$params['action'] = getParamVar( 'action' );
-$params['tmp'] = getParamVar( 'tmp' );
 
-if ( !is_string( $params['list'] ) || $params['list'] == '' ) {
-        die( 'Missing list parameter.' );
+if ( $params['list'] === null || $params['list'] === '' ) {
+	die( 'Missing list parameter.' );
 }
 
-if ( !in_array( $params['action'], $s['validActions'] ) ) {
-        die( 'Invalid action.' );
+if ( !in_array( $params['action'], $validActions ) ) {
+	die( 'Invalid action.' );
 }
 
 /**
@@ -145,29 +138,29 @@ if ( !in_array( $params['action'], $s['validActions'] ) ) {
  */
 
 if ( $params['action'] == 'index' ) {
-        $html = downloadListPage( $params['list'] );
-        $html = injectJQuery( $html );
-        echo $html;
-        die;
+	$html = downloadListPage( $params['list'] );
+	$html = injectJQuery( $html );
+	echo $html;
+	exit;
 }
 
 if ( $params['action'] == 'thismonth' ) {
-        $html = downloadListPage( $params['list'] );
-        $html = injectJQuery( $html );
-        echo injectGoToCurrMonth( $html );
-        die;
+	$html = downloadListPage( $params['list'] );
+	$html = injectJQuery( $html );
+	echo injectGoToCurrMonth( $html, $params['list'] );
+	exit;
 }
 
 if ( $params['action'] == 'lastentry' ) {
-        $html = downloadListPage( $params['list'] );
-        $html = injectJQuery( $html );
-        echo injectGoToLastEntry_StepOne( $html );
-        die;
+	$html = downloadListPage( $params['list'] );
+	$html = injectJQuery( $html );
+	echo injectGoToLastEntry_StepOne( $html, $params );
+	exit;
 }
 
 if ( $params['action'] == 'lastentry-processing' ) {
-        $html = downloadListPage( $params['list'], $params['tmp'] );
-        $html = injectJQuery( $html );
-        echo injectGoToLastEntry_StepTwo( $html );
-        die;
+	$html = downloadListPage( $params['list'], $params['tmp'] );
+	$html = injectJQuery( $html );
+	echo injectGoToLastEntry_StepTwo( $html, $params['list'], $params['tmp'] );
+	exit;
 }
